8a5759151479af74ebfd75c0ee5fb18e18b67f05
darkmatter4114 <darkmatter4114@gmail.com>
2020-04-07 03:04:28 -0700

    sql homework -no commit

--- lantern/relational_database/homework.py
+++ lantern/relational_database/homework.py
@@ -16,13 +16,10 @@ def task_1_add_new_record_to_db(con) -> None:
         """INSERT INTO Customers(CustomerName,ContactName,Address,City,PostalCode,Country) VALUES ('Thomas','David', 'Some Address','London','774','Singapore')""")
 
 
-
-
 def task_2_list_all_customers(cur) -> list:
     cur.execute("""SELECT * FROM Customers ;""")
     list = cur.fetchall()
     return list
-
 
 
 def task_3_list_customers_in_germany(cur) -> list:
@@ -31,20 +28,14 @@ def task_3_list_customers_in_germany(cur) -> list:
     return list
 
 
-
-
 def task_4_update_customer(con):
     cur = con.cursor()
     cur.execute("""UPDATE Customers SET CustomerName='Johnny Depp'  WHERE CustomerID=1; """)
 
 
-
-
 def task_5_delete_the_last_customer(con) -> None:
     cur = con.cursor()
     cur.execute("""DELETE FROM Customers WHERE CustomerID= (SELECT MAX(CustomerID) FROM Customers); """)
-
-
 
 
 def task_6_list_all_supplier_countries(cur) -> list:
@@ -53,14 +44,10 @@ def task_6_list_all_supplier_countries(cur) -> list:
     return list
 
 
-
-
 def task_7_list_supplier_countries_in_desc_order(cur) -> list:
     cur.execute("""SELECT Country FROM Suppliers ORDER BY Country DESC; """)
     list = cur.fetchall()
     return list
-
-
 
 
 def task_8_count_customers_by_city(cur):
@@ -72,16 +59,12 @@ def task_8_count_customers_by_city(cur):
     return list
 
 
-
-
 def task_9_count_customers_by_country_with_than_10_customers(cur):
     cur.execute("""SELECT COUNT(Country), Country FROM Customers 
         GROUP BY Country 
         HAVING COUNT(Country) > 10; """)
     list = cur.fetchall()
     return list
-
-
 
 
 def task_10_list_first_10_customers(cur):
@@ -91,14 +74,11 @@ def task_10_list_first_10_customers(cur):
     return list
 
 
-
-
 def task_11_list_customers_starting_from_11th(cur):
     cur.execute("""SELECT * FROM Customers 
                 OFFSET 11; """)
     list = cur.fetchall()
     return list
-
 
 
 def task_12_list_suppliers_from_specified_countries(cur):
@@ -107,7 +87,6 @@ def task_12_list_suppliers_from_specified_countries(cur):
     or Country='Japan'; """)
     list = cur.fetchall()
     return list
-
 
 
 def task_13_list_products_from_sweden_suppliers(cur):
@@ -119,8 +98,6 @@ def task_13_list_products_from_sweden_suppliers(cur):
     return list
 
 
-
-
 def task_14_list_products_with_supplier_information(cur):
     cur.execute("""SELECT productid, ProductName,Unit,Price,Suppliers.Country,Suppliers.City,Suppliers.SupplierName 
     FROM Products 
@@ -130,15 +107,12 @@ def task_14_list_products_with_supplier_information(cur):
     return list
 
 
-
-
 def task_15_list_customers_with_any_order_or_not(cur):
     cur.execute("""SELECT CustomerName,ContactName,Country,Orders.OrderID
         FROM Customers 
         INNER JOIN Orders ON Orders.CustomerID= Customers.CustomerID; """)
     list = cur.fetchall()
     return list
-
 
 
 def task_16_match_all_customers_and_suppliers_by_country(cur):
