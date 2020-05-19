fb9353eff067e2fc22b6bf774b75067355bea53f
darkmatter4114 <darkmatter4114@gmail.com>
2020-05-19 12:18:38 -0700

    flask add goods view

--- lantern/flask/flask_login_and_templates/practice/grocery_store/commands.py
+++ lantern/flask/flask_login_and_templates/practice/grocery_store/commands.py
@@ -2,16 +2,18 @@ import csv
 import logging
 import os
 
-from grocery_store.models import User, Good, Store
+from grocery_store.models import User, Good, Store, Order, OrderLine
 from grocery_store.config import FIXTURES_DIR, Config
 from sqlalchemy_utils import create_database, drop_database, database_exists
+
+from random import randint, choice, sample, randrange
 
 from flask_script import Command
 
 
-USERS_DIR = os.path.join(FIXTURES_DIR, "users.csv")
-GOODS_DIR = os.path.join(FIXTURES_DIR, "goods.csv")
-STORES_DIR = os.path.join(FIXTURES_DIR, "stores.csv")
+USERS_FILENAME = os.path.join(FIXTURES_DIR, "users.csv")
+GOODS_FILENAME = os.path.join(FIXTURES_DIR, "goods.csv")
+STORES_FILENAME = os.path.join(FIXTURES_DIR, "stores.csv")
 logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s %(levelname)-6s %(message)s",
@@ -21,23 +23,21 @@ logging.basicConfig(
 
 
 def get_users():
-    with open(USERS_DIR, "r") as f:
+    with open(USERS_FILENAME, "r") as f:
         reader = csv.DictReader(f)
         users = [user for user in reader]
-        for user in users:
-            user['password'] = 'sha256$TlFrkAzh$807b3e82dd1b2c6ee26e64ab8ad1ad55fc462d37e9a3b6b5bdd387530867864f'
     return users
 
 
 def get_goods():
-    with open(GOODS_DIR, "r") as f:
+    with open(GOODS_FILENAME, "r") as f:
         reader = csv.DictReader(f)
         goods = [good for good in reader]
     return goods
 
 
 def get_stores():
-    with open(STORES_DIR, "r") as f:
+    with open(STORES_FILENAME, "r") as f:
         reader = csv.DictReader(f)
         stores = [store for store in reader]
     return stores
@@ -61,3 +61,23 @@ class Populate(Command):
                 db.session.add(Store(**store))
             db.session.commit()
             logging.info("Data added to database successfully")
+
+
+class PopulateOrders(Command):
+    def run(self):
+        from grocery_store import app, db
+        with app.app_context():
+            users = User.query.all()
+            goods = Good.query.all()
+            stores = Store.query.all()
+            for user in users:
+                number_of_orders = randint(1, 5)
+                for _ in range(number_of_orders):
+                    number_of_goods = randint(1, 10)
+                    order = Order()
+                    order_lines = [OrderLine(good=good) for good in sample(goods, number_of_goods)]
+                    order.order_lines = order_lines
+                    order.user = user
+                    order.store = choice(stores)
+                    db.session.add(order)
+            db.session.commit()
--- lantern/flask/flask_login_and_templates/practice/grocery_store/routes/main.py
+++ lantern/flask/flask_login_and_templates/practice/grocery_store/routes/main.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from grocery_store.database import db
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 from grocery_store.models import Good, Store
 
@@ -13,6 +13,7 @@ def index():
 
 
 @main.route('/profile')
+@login_required
 def profile():
     return render_template('profile.html', user=current_user.name, email=current_user.email)
 
